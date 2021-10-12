bc4efdb9581a73b582b4c8c6ea44b232ca2b2017
SlothShino <sezeroguzhan26@gmail.com>
2021-10-12 14:59:06 +0300

    Other User Controls Added

---
+++ User_Controls/Institutio_StudentsUserControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Institutio.User_Controls
+{
+    public partial class Institutio_StudentsUserControl : UserControl
+    {
+        public Institutio_StudentsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ User_Controls/Institutio_TeachersUserControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Institutio.User_Controls
+{
+    public partial class Institutio_TeachersUserControl : UserControl
+    {
+        public Institutio_TeachersUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
